ff431503e9bb50abfbdee8276da18c5341d3fb58
Slime_hatena <slime.hatena@gmail.com>
2016-09-08 02:02:22 +0900

    cronを直で叩く対策を一応追加

--- cron.php
+++ cron.php
@@ -1,5 +1,4 @@
 <?php
-
 // cowitter ロード処理
 require dirname ( __FILE__ ) . "/../../undefined/djeetaDefendOrder.php";
 require __DIR__ . '/vendor/autoload.php';
@@ -22,124 +21,132 @@ try {
 }
 // ロード処理ここまで
 
+$debugMode = false;
+if (!isset($_SERVER['REMOTE_ADDR']) || $debugMode) {
+    //これが通ればcronのはず それでも直で叩くアホがいたらbot公開やめる
+    echo "You are cron.";
 
-function checkString($string)
-{
-    // **正規表現であれやこれやして取り出す処理**
-    // 日付を割り出す
-    $pattern0 = "/【グランブルーファンタジー】【DEFEND ORDER】\n.*([0-9]{1,2}\/[0-9]{1,2}).* ([0-9]{1,2}:[0-9]{1,2})より、.*「(.*)」.*\n参加対象はグループ(.*)です。/um";
-    preg_match_all($pattern0, $string, $query_date, PREG_SET_ORDER);
-    
-    var_dump($query_date);
-    
-    foreach ($query_date as $key => $value) {
-        $replaceQuery[$key]["string"] = $value[0];
-        $replaceQuery[$key]["time"] =     strtotime(date("Y",strtotime("-1 day")) . "/" . $value[1] . " " . $value[2]);
-        $replaceQuery[$key]["time_str"]  = date('Y/m/d H:i', $replaceQuery[$key]["time"]);
-        $replaceQuery[$key]["place"] = $value[3];
-        $replaceQuery[$key]["group"] = $value[4];
-        $replaceQuery[$key]["group"] = str_replace("/" , "" ,  $replaceQuery[$key]["group"]);
-        $replaceQuery[$key]["group"] = str_replace("#グラブル" , "" ,  $replaceQuery[$key]["group"]);
-    }
-    var_dump ($replaceQuery);
-    echo "<hr>";
-    return $replaceQuery;
-}
-
-function getGbfNotice($client){
-    // **ツイートを取得して配列化する**
-    $statuses = $client->get('search/tweets', ['q' => 'from:granbluefantasy 【グランブルーファンタジー】【DEFEND ORDER】 にて防衛戦が発生します。 #グラブル '])->statuses;
-    foreach ($statuses as $key => $value) {
-        $tweets_gbf_notice[$key] = array(
-        "id" => $value -> id_str,
-        "time" => strtotime($value -> created_at),
-        "text" => $value -> text
-        );
-    }
-    
-    return $tweets_gbf_notice;
-}
-
-
-function postTweetAll($time, $time_str, $place, $group, $client){
-    // **ALL用のアカウントにツイートさせる処理**
-    $difference = $time - time() + 30;  //ラグ考慮して+30s
-    echo $difference . "<br>";
-    
-    $tweetFlag = false;
-    
-    if ($difference >= 0  && $difference < 3000){
-        $tweet_time =  preg_replace('/^0/','',date("i分後",$difference)); //なぜ時刻の書式に0埋めなし分が無いのか
-        $tweetFlag = true;
-    }else{
-        echo "きっと時間外だね ツイートはしないよ";
-    }
-    if($tweetFlag){
-        // グループを記号で区切る
-        $groupArr = str_split($group);
-        $tweetGroup = "";
-        foreach ($groupArr as $value) {
-            $tweetGroup = $tweetGroup . $value . "/";
-        }
-        $tweetGroup = substr($tweetGroup, 0, -1);
+    function checkString($string)
+    {
+        // **正規表現であれやこれやして取り出す処理**
+        // 日付を割り出す
+        $pattern0 = "/【グランブルーファンタジー】【DEFEND ORDER】\n.*([0-9]{1,2}\/[0-9]{1,2}).* ([0-9]{1,2}:[0-9]{1,2})より、.*「(.*)」.*\n参加対象はグループ(.*)です。/um";
+        preg_match_all($pattern0, $string, $query_date, PREG_SET_ORDER);
         
-        $tweetString =
-        "【防衛戦発生予告】[". $time_str ."]\n約" . $tweet_time. "「" . $place . "」にて発生します。\n対象グループ : " . $tweetGroup . " #グラブル #ディフェンドオーダー";
-        echo "<pre>" . $tweetString . "</pre>";
+        var_dump($query_date);
+        
+        foreach ($query_date as $key => $value) {
+            $replaceQuery[$key]["string"] = $value[0];
+            $replaceQuery[$key]["time"] =     strtotime(date("Y",strtotime("-1 day")) . "/" . $value[1] . " " . $value[2]);
+            $replaceQuery[$key]["time_str"]  = date('Y/m/d H:i', $replaceQuery[$key]["time"]);
+            $replaceQuery[$key]["place"] = $value[3];
+            $replaceQuery[$key]["group"] = $value[4];
+            $replaceQuery[$key]["group"] = str_replace("/" , "" ,  $replaceQuery[$key]["group"]);
+            $replaceQuery[$key]["group"] = str_replace("#グラブル" , "" ,  $replaceQuery[$key]["group"]);
+        }
+        var_dump ($replaceQuery);
+        echo "<hr>";
+        return $replaceQuery;
+    }
+    
+    function getGbfNotice($client){
+        // **ツイートを取得して配列化する**
+        $statuses = $client->get('search/tweets', ['q' => 'from:granbluefantasy 【グランブルーファンタジー】【DEFEND ORDER】 にて防衛戦が発生します。 #グラブル '])->statuses;
+        foreach ($statuses as $key => $value) {
+            $tweets_gbf_notice[$key] = array(
+            "id" => $value -> id_str,
+            "time" => strtotime($value -> created_at),
+            "text" => $value -> text
+            );
+        }
+        
+        return $tweets_gbf_notice;
+    }
+    
+    
+    function postTweetAll($time, $time_str, $place, $group, $client){
+        // **ALL用のアカウントにツイートさせる処理**
+        $difference = $time - time() + 30;  //ラグ考慮して+30s
+        echo $difference . "<br>";
+        
+        $tweetFlag = false;
+        
+        if ($difference >= 0  && $difference < 3000){
+            $tweet_time =  preg_replace('/^0/','',date("i分後",$difference)); //なぜ時刻の書式に0埋めなし分が無いのか
+            $tweetFlag = true;
+        }else{
+            echo "きっと時間外だね ツイートはしないよ";
+        }
+        if($tweetFlag){
+            // グループを記号で区切る
+            $groupArr = str_split($group);
+            $tweetGroup = "";
+            foreach ($groupArr as $value) {
+                $tweetGroup = $tweetGroup . $value . "/";
+            }
+            $tweetGroup = substr($tweetGroup, 0, -1);
+            
+            $tweetString =
+            "【防衛戦発生予告】[". $time_str ."]\n約" . $tweet_time. "「" . $place . "」にて発生します。\n対象グループ : " . $tweetGroup . " #グラブル #ディフェンドオーダー";
+            echo "<pre>" . $tweetString . "</pre>";
+            $client->post('statuses/update', ['status' =>$tweetString]);
+            echo "Allアカウント : ツイート<br>";
+            
+            
+            //各グループアカウントでツイート
+            foreach ($groupArr as $value) {
+                postTweetGroup($value ,$tweetString);
+                echo $value . "アカウント : ツイート<br>";
+            }
+        }
+    }
+    
+    function postTweetGroup($group, $tweetString){
+        require dirname ( __FILE__ ) . "/../../undefined/djeetaDefendOrder.php";
+        //ユーザーを変えるので新しくロード
+        $client = new Client([$consumer_key, $consumer_secret,
+        $TwitterUsers[$group]['access_token'], $TwitterUsers[$group]['access_token_secret']]);
+        $client = $client->withOptions([CURLOPT_CAINFO => __DIR__ . '/vendor/cacert.pem']);
         $client->post('statuses/update', ['status' =>$tweetString]);
-        echo "Allアカウント : ツイート<br>";
+    }
+    
+    // 取得してチェックさせる
+    $tweets_gbf_notice = getGbfNotice($client); //ここクライアントを送ってあげないと取得できないので
+    foreach ($tweets_gbf_notice as $key => $value) {
+        $gbf_notice_group[$key] = checkString($value["text"]);
+    }
+    
+    // データベースに入れる処理
+    foreach ($gbf_notice_group as $key => $value_n) {
         
-        
-        //各グループアカウントでツイート
-        foreach ($groupArr as $value) {
-            postTweetGroup($value ,$tweetString);
-            echo $value . "アカウント : ツイート<br>";
+        foreach ($value_n as $key => $value) {
+            $sql = "INSERT IGNORE  INTO `djeetadefendorder` (`time`, `time_str`, `group`, `place`) values (?,?,?,?)";
+            $stmt=$pdo->prepare($sql);
+            $res=$stmt->execute(array($value["time"], $value["time_str"], $value["group"] ,$value["place"]));
+            echo "<pre>" . "time : " . $value["time"] .
+            "<br>time_str : " . $value["time_str"] .
+            "<br>group : " . $value["group"] .
+            "<br>place : " . $value['place'] .
+            "</pre>";
+            sleep(0.5);
         }
-    }
-}
-
-function postTweetGroup($group, $tweetString){
-    require dirname ( __FILE__ ) . "/../../undefined/djeetaDefendOrder.php";
-    //ユーザーを変えるので新しくロード
-    $client = new Client([$consumer_key, $consumer_secret,
-    $TwitterUsers[$group]['access_token'], $TwitterUsers[$group]['access_token_secret']]);
-    $client = $client->withOptions([CURLOPT_CAINFO => __DIR__ . '/vendor/cacert.pem']);
-    $client->post('statuses/update', ['status' =>$tweetString]);
-}
-
-// 取得してチェックさせる
-$tweets_gbf_notice = getGbfNotice($client); //ここクライアントを送ってあげないと取得できないので
-foreach ($tweets_gbf_notice as $key => $value) {
-    $gbf_notice_group[$key] = checkString($value["text"]);
-}
-
-// データベースに入れる処理
-foreach ($gbf_notice_group as $key => $value_n) {
-    
-    foreach ($value_n as $key => $value) {
-        $sql = "INSERT IGNORE  INTO `djeetadefendorder` (`time`, `time_str`, `group`, `place`) values (?,?,?,?)";
-        $stmt=$pdo->prepare($sql);
-        $res=$stmt->execute(array($value["time"], $value["time_str"], $value["group"] ,$value["place"]));
-        echo "<pre>" . "time : " . $value["time"] .
-        "<br>time_str : " . $value["time_str"] .
-        "<br>group : " . $value["group"] .
-        "<br>place : " . $value['place'] .
-        "</pre>";
-        sleep(0.5);
+        
     }
     
+    echo "<hr>";
+    
+    $time = time();
+    
+    // データーベースから直近の内容を抜き出す
+    $sql = "SELECT  `time` ,`time_str` , `group`, `place`  FROM `djeetadefendorder` WHERE `time` <=  ? AND `time` >=  ?";
+    $stmt=$pdo->prepare($sql);
+    $res=$stmt->execute(array($time  + 113610 , $time - 10));
+    
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    var_dump($result);
+    
+    postTweetAll($result['time'], $result['time_str'], $result['place'] ,$result['group'], $client);
+    
+}else{
+    echo '<meta http-equiv="refresh" content="1; URL=index.php">';
 }
-
-echo "<hr>";
-
-$time = time();
-
-// データーベースから直近の内容を抜き出す
-$sql = "SELECT  `time` ,`time_str` , `group`, `place`  FROM `djeetadefendorder` WHERE `time` <=  ? AND `time` >=  ?";
-$stmt=$pdo->prepare($sql);
-$res=$stmt->execute(array($time  + 113610 , $time - 10));
-
-$result = $stmt->fetch(PDO::FETCH_ASSOC);
-var_dump($result);
-
-postTweetAll($result['time'], $result['time_str'], $result['place'] ,$result['group'], $client);
--- index.php
+++ index.php
@@ -1,84 +1,61 @@
 <?php
-require_once dirname ( __FILE__ ) . "/../../undefined/DSN.php";
-try {
-    $pdo = new PDO ( 'mysql:host=' . $dsn ['host'] . ';dbname=' . $dsn ['dbname'] . ';charset=utf8', $dsn ['user'], $dsn ['pass'], array (
-    PDO::ATTR_EMULATE_PREPARES => false,
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    ) );
-} catch ( PDOException $e ) {
-    exit ( 'connection unsuccess' . $e->getMessage () );
-}
-
-echo '
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="style.css">
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="lib/animatedtablesorter/tsort.js"></script>
-<script src="lib/animatedtablesorter/setting.js"></script>
-<title>ディフェンドオーダー発生履歴 - svr.aki-memo.net</title>
-</head>
-<body>
-';
-
-$GLOBALS['count'] = ["A" => 0, "B" => 0, "C" => 0, "D" => 0, "E" => 0, "F" => 0, "G" => 0, "H" => 0];;
-
-function doHolding($groupString, $group){
-    if(strpos($groupString, $group) !== false){
-        $return[$group] = "■";
-        $GLOBALS['count'][$group]++;
-    }else{
-        $return[$group] = "";
+    require_once dirname ( __FILE__ ) . "/../../undefined/DSN.php";
+    try {
+        $pdo = new PDO ( 'mysql:host=' . $dsn ['host'] . ';dbname=' . $dsn ['dbname'] . ';charset=utf8', $dsn ['user'], $dsn ['pass'], array (
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ) );
+    } catch ( PDOException $e ) {
+        exit ( 'connection unsuccess' . $e->getMessage () );
     }
-    return $return;
-}
-
-$sql = "SELECT * FROM `djeetadefendorder` ORDER BY `time`";
-$stmt=$pdo->prepare($sql);
-$res=$stmt->execute();
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-print<<<EOF
-
-<h1>ディフェンドオーダー開催履歴(仮)</h1>
-<p>取り敢えず作りました。<br>botが稼働してからのデータのみです。気が向けば過去のデータも取得するかもしれません。<br>
-表の一番上を押すとソートできます。<br>
-<a href="http://aki-memo.net/defendorderbot" target="_blank">Botについてはこちら</a></p>
-
-<table class="tableSorter">
-<thead>
-<tr><th>時間</th><th>場所</th><th>A</th><th>B</th><th>C</th><th>D</th><th>E</th><th>F</th><th>G</th><th>H</th>
-</thead>
-<tbody>
-EOF;
-
-$allGroup = ["A", "B", "C", "D", "E", "F", "G", "H"];      //foreachとかで回せると楽なので
-$groupStringArr = [
-'A' => "",
-'B' => "",
-'C' => "",
-'D' => "",
-'E' => "",
-'F' => "",
-'G' => "",
-'H' => "",
-]; //ここをhtmlで出力する
-
-foreach ($result as $value) {
     
-    foreach ($allGroup as $key) {
-        $groupStringArr = array_merge($groupStringArr, doHolding($value["group"], $key));
-    }
-    //ここから１列出力
-    echo '<tr><td>' . $value['time_str'] .  '</td><td>' . $value['place'] . '</td>';
-    foreach ($allGroup as $gs) {
-        echo '<td data-sortAs="' . $groupStringArr[$gs] . "-" .  $value['time'] .  '">' . $groupStringArr[$gs] . "</td>";
-    }
-    echo "</tr>";
+    echo '
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="lib/animatedtablesorter/tsort.js"></script>
+    <script src="lib/animatedtablesorter/setting.js"></script>
+    <title>ディフェンドオーダー発生履歴 - svr.aki-memo.net</title>
+    </head>
+    <body>
+    ';
     
-    $groupStringArr = [ //初期化して終わり
+    $GLOBALS['count'] = ["A" => 0, "B" => 0, "C" => 0, "D" => 0, "E" => 0, "F" => 0, "G" => 0, "H" => 0];;
+    
+    function doHolding($groupString, $group){
+        if(strpos($groupString, $group) !== false){
+            $return[$group] = "■";
+            $GLOBALS['count'][$group]++;
+        }else{
+            $return[$group] = "";
+        }
+        return $return;
+    }
+    
+    $sql = "SELECT * FROM `djeetadefendorder` ORDER BY `time`";
+    $stmt=$pdo->prepare($sql);
+    $res=$stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    echo '
+    
+    <h1>ディフェンドオーダー開催履歴(仮)</h1>
+    <p>取り敢えず作りました。<br>botが稼働してからのデータのみです。気が向けば過去のデータも取得するかもしれません。<br>
+    表の一番上を押すとソートできます。<br>
+    <a href="http://aki-memo.net/defendorderbot" target="_blank">Botについてはこちら</a></p>
+    
+    <table class="tableSorter">
+    <thead>
+    <tr><th>時間</th><th>場所</th><th>A</th><th>B</th><th>C</th><th>D</th><th>E</th><th>F</th><th>G</th><th>H</th>
+    </thead>
+    <tbody>
+    ';
+    
+    $allGroup = ["A", "B", "C", "D", "E", "F", "G", "H"];      //foreachとかで回せると楽なので
+    $groupStringArr = [
     'A' => "",
     'B' => "",
     'C' => "",
@@ -87,15 +64,38 @@ foreach ($result as $value) {
     'F' => "",
     'G' => "",
     'H' => "",
-    ];
-}
-
-echo '</tbody></table>';
-
-echo "<p>";
-foreach ($allGroup as $key) {
-    echo $key . "グループ：" . $GLOBALS['count'][$key] . "回<br>";
-}
-echo "</p>";
-
-echo "</body></html>";
+    ]; //ここをhtmlで出力する
+    
+    foreach ($result as $value) {
+        
+        foreach ($allGroup as $key) {
+            $groupStringArr = array_merge($groupStringArr, doHolding($value["group"], $key));
+        }
+        //ここから１列出力
+        echo '<tr><td>' . $value['time_str'] .  '</td><td>' . $value['place'] . '</td>';
+        foreach ($allGroup as $gs) {
+            echo '<td data-sortAs="' . $groupStringArr[$gs] . "-" .  $value['time'] .  '">' . $groupStringArr[$gs] . "</td>";
+        }
+        echo "</tr>";
+        
+        $groupStringArr = [ //初期化して終わり
+        'A' => "",
+        'B' => "",
+        'C' => "",
+        'D' => "",
+        'E' => "",
+        'F' => "",
+        'G' => "",
+        'H' => "",
+        ];
+    }
+    
+    echo '</tbody></table>';
+    
+    echo "<p>";
+    foreach ($allGroup as $key) {
+        echo $key . "グループ：" . $GLOBALS['count'][$key] . "回<br>";
+    }
+    echo "</p>";
+    
+    echo "</body></html>";
